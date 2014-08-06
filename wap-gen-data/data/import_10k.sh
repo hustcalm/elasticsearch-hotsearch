7cb0e5919a0312885b0984011f8e37cfbb02317c
landhero <raindrops_fall@sina.com>
2014-08-06 12:51:52 +0800

    add script and refresh data

---
+++ wap-gen-data/data/import_10k.sh
@@ -0,0 +1,3 @@
+curl -XPUT 'http://localhost:9200/query_data_10k/'
+curl -XPUT 'http://localhost:9200/query_data_10k/query/_mapping' -d @mapping.json
+curl -s -XPOST 'http://localhost:9200/_bulk' --data-binary @data.json
--- wap-gen-data/src/com/cgroups/gendata/GenerateData.java
+++ wap-gen-data/src/com/cgroups/gendata/GenerateData.java
@@ -15,11 +15,11 @@ public class GenerateData {
 		try{ 
 			bw = new BufferedWriter(new FileWriter("data/queries.csv"));
 			data_bw = new BufferedWriter(new FileWriter("data/data.json"));
-			for (int i=1; i<=1000; i++){
+			for (int i=1; i<=10000; i++){
 				Query query = qg.getNextQuery();
 				bw.write(query.toString());
 				StringBuilder sb = new StringBuilder();
-				sb.append(String.format("{\"index\":{\"_index\":\"query_data_1k\",\"_type\":\"query\",\"_id\":\"%d\"}}\n",i));
+				sb.append(String.format("{\"index\":{\"_index\":\"query_data_10k\",\"_type\":\"query\",\"_id\":\"%d\"}}\n",i));
 				sb.append(String.format("{\"date\":\"%s\",\"city\":\"%s\",\"word\":\"%s\",\"status\":0}\n", 
 						Helper.getDayStringOfDate(query.date), query.city, query.content));
 				data_bw.write(sb.toString());
