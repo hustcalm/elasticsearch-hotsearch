9644c091c433e14f2a5c008dbc68c24c01fa7ad8
landhero <raindrops_fall@sina.com>
2014-08-06 14:06:19 +0800

    update mapping

--- wap-gen-data/data/import_10k.sh
+++ wap-gen-data/data/import_10k.sh
@@ -1,3 +1,4 @@
+curl -XDELETE 'http://localhost:9200/query_data_10k/'
 curl -XPUT 'http://localhost:9200/query_data_10k/'
 curl -XPUT 'http://localhost:9200/query_data_10k/query/_mapping' -d @mapping.json
 curl -s -XPOST 'http://localhost:9200/_bulk' --data-binary @data.json
---
+++ wap-gen-data/src/com/cgroups/gendata/elacontrol/ElasticController.java
@@ -0,0 +1,3 @@
+package com.cgroups.gendata.elacontrol;
+public class ElasticController {
+}
