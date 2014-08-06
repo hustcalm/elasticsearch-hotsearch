77f0cd2cf1be1b62959864a4efdc7f1f7812bb84
hustcalm <licalmer@gmail.com>
2014-08-06 17:15:19 +0900

    bye, the old webapp

--- src/main/webapp/js/app/indexInspectorConstants.js
+++
@@ -1,7 +0,0 @@
-var com = com || {};
-com.worksap = com.worksap || {};
-com.worksap.elasticsearch = com.worksap.elasticsearch || {};
-
-com.worksap.elasticsearch.Constants = {
-		ID_KEY : "_index_inspector_id",
-};
